131327d147410a052883a38c11a925e90e5bde2a
VitaliiSavelyev <BinT0.5@mail.ru>
2021-08-02 01:16:13 +0300

    feat: add interfaces

--- rs-angular-create/src/app/search/search-item.model.ts
+++ rs-angular-create/src/app/search/search-item.model.ts
@@ -1,2 +1,18 @@
 export interface SearchItem {
+  kind: string,
+  etag: string,
+  id: string,
+  snippet: {
+    publishedAt: string;
+    channelId: string;
+    title: string;
+    description: string;
+    thumbnails: {
+      typeVideo: {
+        url: string,
+        width: number,
+        height: number
+      }
+    }
+  }
 }
--- rs-angular-create/src/app/search/search-response.model.ts
+++ rs-angular-create/src/app/search/search-response.model.ts
@@ -1,2 +1,5 @@
 export interface SearchResponse {
+  kind: string,
+  etag: string,
+  pageInfo: { totalResults: number; resultsPerPage: number }
 }
